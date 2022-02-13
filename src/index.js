e0c29e41410a23451d0a4104e63a4f9dba0383ef
ifed91 <i.fed91@gmail.com>
2022-02-13 23:53:58 +0300

    docs:change

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // your solution 
+ let stringStr = str;
+  const brackets = [];
+  bracketsConfig.forEach(bracket => {
+    brackets.push(bracket.join(''))
+  });
+  for (let i = 0; i < brackets.length;) {
+    if (stringStr.includes(brackets[i])) {
+      stringStr = stringStr.replace(brackets[i], '');
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+  return stringStr ? false : true;
 }
